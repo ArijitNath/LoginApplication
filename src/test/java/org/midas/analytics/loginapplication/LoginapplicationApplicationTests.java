bf361b11344af944bd2cb1d95a2e921f3e68cf9f
Arijit Nath <arijitnath92@gmail.com>
2020-07-14 21:44:03 +0530

    remove test

--- src/test/java/org/midas/analytics/loginapplication/LoginapplicationApplicationTests.java
+++ src/test/java/org/midas/analytics/loginapplication/LoginapplicationApplicationTests.java
@@ -3,10 +3,10 @@ package org.midas.analytics.loginapplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
 class LoginapplicationApplicationTests {
 
-	@Test
+	
 	void contextLoads() {
 	}
 
